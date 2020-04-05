95074ccfa608b0d660cbf2c101f95bb4858870a3
LauraYG <100363785@alumnos.uc3m.es>
2020-04-05 19:12:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lsi_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lsi_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
